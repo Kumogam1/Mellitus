2586f91abde88e16024c91bfdd1b4281c996f4dc
Edouard Gu <e.gu@unistra.fr>
2018-10-15 16:34:49 +0200

    Update affichageStats

--- affichageStats.js
+++ affichageStats.js
@@ -11,30 +11,21 @@ exports.graphString = function(bornMax, bornMin, nbLignes, tabDonnes) {
   const tabValLigne = [];
   let res = '';
   for (let i = 0; i < nbLignes; i++) {
-    tabValLigne.push(bornMin + (divEucli(equartBornes, nbLignes) * i));
+    tabValLigne.push(bornMin + (Math.round(equartBornes / nbLignes) * i));
   }
   console.log(tabValLigne);
   for (let i = nbLignes - 1; i >= 0; i--) {
     res = res + '>' + tabValLigne[i] + '\t';
-    for (let value in tabDonnes) {
-      if (value > tabValLigne[i]) {
+    tabDonnes.forEach(function(value) {
+      if (value >= tabValLigne[i]) {
         res = res + '▮ ';
       }
       else {
         res = res + '▯ ';
       }
-    }
+    });
     res = res + '\n';
   }
   console.log(res);
   return res;
 };
-
-
-// division euclidienne de a par b
-function divEucli(a, b) {
-  const reste = a % b;
-  const newA = a - reste;
-  const res = newA / b;
-  return res;
-}
