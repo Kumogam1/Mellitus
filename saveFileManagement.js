69a28f35e09b1ed2f6f7fc5f4b4fd8a510fa0843
Pierre Lenertz <plenertz@etu.unistra.fr>
2018-10-15 10:32:36 +0200

    Ajout de fonction pour sauver et charger une partie

---
+++ saveFileManagement.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+/**
+* loadSave : function pour recuperer l'ojet de sauvegarde deupuis le lireFichier
+* userId : id du joueur
+**/
+exports.loadSave = function(userId) {
+  let save = fs.readFileSync('./sauvegardesPartie/' + userId + '.json');
+  save = JSON.parse(save);
+  return save;
+};
+
+/**
+* save : function pour enrengister la sauvegarde modifiée
+* userId : id du joueur
+* partie : objet json de la partie
+**/
+exports.save = function save(userId, partie) {
+  fs.writeFileSync('./sauvegardesPartie/' + userId + '.json', JSON.stringify(partie, null, 2), function(err) {
+    if (err) throw err;
+    console.log('Save file updatd for ' + userId);
+  });
+};
