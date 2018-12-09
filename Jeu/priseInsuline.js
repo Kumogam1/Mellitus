9dc8cd08636e69b6663c1bab9b2c50b0fa006ed0
Thomas Mayer <tmayer@etu.unistra.fr>
2018-12-09 18:27:15 +0100

    Verification des input

--- Jeu/priseInsuline.js
+++ Jeu/priseInsuline.js
@@ -8,6 +8,8 @@ const config = require("./token.json");
 const client = new Discord.Client();
 client.login(config.token);
 
+const tabNb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 exports.priseInsuline = function priseInsuline(message, partie) {
 
   let insuline = '-1';
@@ -26,12 +28,22 @@ exports.priseInsuline = function priseInsuline(message, partie) {
 
     if(message.member.roles.some(r=>['Joueur'].includes(r.name)))
     {
-
+      let bool = true;
       if (partie.insuline == 1)
       {
+        if(message.content.length == 1 || message.content.length == 2){
+          for(let i = 0; i < message.content.length; i++){
+            if(!tabNb.includes(message.content.charAt(i))){
+              bool = false;
+            }
+          }
+        }
+        else{
+          bool = false;
+        }
         insuline = parseInt(message.content);
 
-        if(Number.isInteger(insuline))
+        if(bool == true)
         {
           if(insuline < 0 || insuline > 80 || isNaN(insuline))
           {
@@ -44,6 +56,9 @@ exports.priseInsuline = function priseInsuline(message, partie) {
             sfm.save(message.author.id, partie);
             message.react('➡');
           }
+        }
+        else{
+          message.channel.send("Mettez-y du votre aussi !")
         }
       }
     }
